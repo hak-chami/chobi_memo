b3f7f88507795125769977d00460b43814dc23c1
hiroko <tora.cnch0205@gmail.com>
2018-05-20 10:18:00 +0900

    wordsモデルとコントローラの作成
    wordsモデルとコントローラの作成
    モデルの関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_13_132238) do
+ActiveRecord::Schema.define(version: 2018_05_20_003851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2018_05_13_132238) do
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "word"
+    t.string "kana"
+    t.text "content"
+    t.text "content_replace"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
