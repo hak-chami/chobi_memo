8cd9e97ee09c98f2e31c265cde7aee0708fde1a5
hiroko <tora.cnch0205@gmail.com>
2018-05-22 23:25:48 +0900

    登録語詳細ページ作成
    登録語詳細ページ
    登録語削除機能
    一覧ページを登録日時降順表示に変更

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,10 +1,12 @@
 class WordsController < ApplicationController
+  before_action :set_word, only: [:show, :destroy]
+
   def user
     @user = current_user
   end
 
   def index
-    @words = self.user.words.page(params[:page]).per(24)
+    @words = self.user.words.order(created_at: :desc).page(params[:page]).per(24)
   end
 
   def new
@@ -20,9 +22,21 @@ class WordsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def destroy
+    @word.destroy
+    redirect_to words_path, alert: '単語を削除しました'
+  end
+
   private
 
   def words_params
       params.require(:word).permit(:word, :kana, :content)
+  end
+
+  def set_word
+    @word = self.user.words.find_by(id: params[:id])
   end
 end
