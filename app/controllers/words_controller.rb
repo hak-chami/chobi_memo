13e644458cdbe638bb4c3241a852e314ed3f40b3
hiroko <tora.cnch0205@gmail.com>
2018-05-23 21:25:40 +0900

    登録語編集機能作成
    登録語編集ページ作成
    フォームをパーシャルに切り分け

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,17 +1,17 @@
 class WordsController < ApplicationController
-  before_action :set_word, only: [:show, :destroy]
+  before_action :set_word, only: [:show, :destroy, :edit, :update]
 
   def index
     @words = current_user.words.order(created_at: :desc).page(params[:page]).per(24)
   end
 
   def new
-    @words = current_user.words.new
+    @word = current_user.words.new
   end
 
   def create
-    @words = current_user.words.new(words_params)
-    if @words.save
+    @word = current_user.words.new(words_params)
+    if @word.save
       redirect_to words_path, notice: '単語を登録しました！'
     else
       render 'new'
@@ -24,6 +24,17 @@ class WordsController < ApplicationController
   def destroy
     @word.destroy
     redirect_to words_path, alert: '単語を削除しました'
+  end
+
+  def edit
+  end
+
+  def update
+    if @word.update(words_params)
+      redirect_to word_path, notice: '単語を編集しました！'
+    else
+      render 'show'
+    end
   end
 
   private
